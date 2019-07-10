810254daf7a3ae1a484a9be29d6ad2900213287a
Ekaterina Smirnova <smkate.work@gmail.com>
2019-07-11 00:58:13 +0300

    Start Axios

--- src/components/pages/Catalog.vue
+++ src/components/pages/Catalog.vue
@@ -2,6 +2,7 @@
   <div>
     <h2>Ama Catalog</h2>
     <div class="catalog-list">
+      <pre style="color: #ffffff;">{{ info }}</pre>
       <!--<catalog-entry></catalog-entry>-->
       <!--<catalog-entry></catalog-entry>-->
       <!--<catalog-entry></catalog-entry>-->
@@ -11,11 +12,21 @@
 
 <script>
 import Vue from 'vue'
+import Axios from 'axios';
 import CatalogEntry from '../common/CatalogEntry.vue'
 
-Vue.component('catalog-entry', {})
+//Vue.component('catalog-entry', {})
 
 export default {
-
+  data() {
+    return {
+      info: null
+    }
+  },
+  mounted() {
+    Axios
+      .get('http://jsonplaceholder.typicode.com/users')
+      .then(response => (this.info = response));
+  }
 }
 </script>
