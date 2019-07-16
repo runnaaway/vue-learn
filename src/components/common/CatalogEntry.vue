f8592eed45273f764bbca57bda6a4b8e8ee9ac47
runnaaway <smkate.work@gmail.com>
2019-07-16 18:14:27 +0300

    End entries, start filter (need a new component?)

--- src/components/common/CatalogEntry.vue
+++ src/components/common/CatalogEntry.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="catalog-entry">
     <div class="entry__photo">
-      <img src="http://lorempixel.com/200/200/" alt=""/>
+      <img :src="photo" alt=""/>
     </div>
     <div class="entry__name">{{name}}</div>
-    <div class="entry__email"><a :href="'mailto:' + email"></a></div>
-    <div class="entry__phone"><a :href="'tel:' + phone"></a></div>
-    <div class="entry__address">{{address}}</div>
-    <a :href="'mailto:' + email" class="btn btn-success">Contact</a>
+    <div class="entry__phone">Phone: <a :href="'tel:' + phone">{{phone}}</a></div>
+    <div class="entry__phone">Website: <a :href="'http://' + website">{{website}}</a></div>
+    <div class="entry__address">Address: {{address}}</div>
+    <div class="flex justify-content-between">
+      <a href="#" :data-coords="coords" class="btn btn-sm btn-primary">View map</a>
+      <a :href="'mailto:' + email" class="btn btn-sm btn-success">Contact</a>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['name', 'email', 'phone', 'address']
+  props: ['coords', 'photo', 'name', 'email', 'phone', 'website', 'address']
 }
 </script>
--- src/components/pages/Catalog.vue
+++ src/components/pages/Catalog.vue
@@ -1,13 +1,32 @@
 <template>
   <div>
     <h2>Ama Catalog</h2>
+      <div class="catalog-filter">
+          <div class="filter__title">Filter:</div>
+          <label class="filter__label">
+              <input type="checkbox" name="website" :value="all">
+              <span>All</span>
+          </label>
+          <label v-for="item in filter" class="filter__label">
+              <input type="checkbox" name="website" :value="item" v-model="checked">
+              <span>{{item}}</span>
+          </label>
+          <div class="filter__title">Search: </div>
+          <input class="form-control" type="text" v-model="searchFilter">
+      </div>
+      {{checked}}
     <div class="catalog-list">
-<!--      <pre style="color: #ffffff;">{{ info[0].address }}</pre>-->
-      <catalog-entry v-for="(item) in info"
+<!--      <pre style="color: #ffffff;">{{ info }}</pre>-->
+      <catalog-entry v-for="(item, i) in info"
                      :key="item.id"
+                     :photo="photo(i)"
+                     :coords="coords(i)"
+                     :img="item.photo"
+                     :website="item.website"
                      :name="item.name"
                      :email="item.email"
                      :phone="item.phone"
+                     :address="address(i)"
       ></catalog-entry>
     </div>
   </div>
@@ -20,19 +39,32 @@ import CatalogEntry from '../common/CatalogEntry.vue'
 export default {
   data() {
     return {
-      info: null
+      info: null,
+      filter: ['org', 'net', 'info', 'biz', 'io', 'com'],
+      all: '',
+      searchFilter: ''
     }
   },
   mounted() {
     Axios
       .get('http://jsonplaceholder.typicode.com/users')
-      .then(response => (this.info = response.data));
+      .then(response => {this.info = response.data});
   },
   components: {
     CatalogEntry
   },
-  computed: {
-
+  methods: {
+    address(index) {
+      let address = this.info[index].address.city + ', ' + this.info[index].address.street + ' ' + this.info[index].address.suite;
+      return address
+    },
+    coords(index) {
+      let coords = this.info[index].address.geo.lat + ', ' + this.info[index].address.geo.lng;
+      return coords
+    },
+    photo(index) {
+      return 'https://picsum.photos/id/' + index + '/200'
+    }
   }
 }
 </script>
